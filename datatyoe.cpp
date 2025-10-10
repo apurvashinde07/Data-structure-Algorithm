aadfc52a8bdd8fd44b9c40d4bb09237e480aa1c4
Apurva shinde <apurvashinde077@gmail.com>
2025-10-10 22:54:19 +0530

    Add files via upload

---
+++ datatyoe.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(long int)<<endl;
+    cout<<sizeof(long long int)<<endl;
+    cout<<sizeof(short int)<<endl;
+    unsigned int x = -45;
+    cout<<x<<endl;
+    signed int y= -27;
+    cout<<y<<endl;
+    return 0;
+}
